06bb98a7e43a83b5d97987b48961b34d2bb86d60
Joseph E <jemsley0@gmail.com>
2021-05-04 10:06:40 -0400

    Clean Up

--- fRankenmovie.R
+++ fRankenmovie.R
@@ -1,10 +1,28 @@
+### About ----
+
+# The Dataset
+
+# The Goal
+
+# The Methodology
+
+### package load and script set up ----
+
 # load pacman
 library(pacman)
 
 # use pacman package to load other packages
 p_load(tidyr, rio, dplyr, tidyverse)
 
-setpath <- "C:/Users/JosephEmsley/Dropbox (AMEND Consulting)/AMEND Internal Business/So What/2021_q1_movies/Data"
+### variable setting ----
+
+setpath <- "C:/Users/JosephEmsley/Documents/Github/sowhat-fRankenmovie/Data"
+
+### ETL ----
+
+# the movies we will be using must fit certain criteria
+#   1. a USA based film (to account for "market reach" of movies as it would apply to gross)
+#   2. a 20 year time period (1999-2019)
 
 imdb_movies <- read.csv(paste(setpath,"imdb movies.csv", sep="/"), header = TRUE) %>% 
   mutate(year = as.numeric(substr(year, nchar(year)-4+1,nchar(year))),
@@ -14,12 +32,17 @@ imdb_movies <- read.csv(paste(setpath,"imdb movies.csv", sep="/"), header = TRUE
          usa_gross_income = as.numeric(trimws(str_remove_all(usa_gross_income,"[^[:alnum:]]"))),
          worldwide_gross = ifelse(is.na(worlwide_gross_income),usa_gross_income,worlwide_gross_income)) %>%
   separate(genre, c("genre2","genre3","genre4"), sep = " ", remove = FALSE) %>% 
-  filter(grepl("USA",country)) %>% 
+  separate(country, "usacheck", sep = " ", remove = FALSE) %>% 
+  mutate(usacheck = trimws(str_remove_all(usacheck,"[^[:alnum:]]"))) %>% 
+  filter(usacheck == "USA") %>% 
   filter(year >= 1999) %>% 
   filter(year <= 2019) %>% 
   filter(!is.na(worldwide_gross)) %>% 
   separate(actors, c("lead","supporting"), sep = ", ", remove = FALSE)
 
+### Analysis ----
+
+# see which leads are related to the highest gross
 lm_lead <- lm(worldwide_gross ~ lead, data = imdb_movies)
 lm_anova <- anova(lm_lead)
 lm_summary <- summary(lm_lead)
@@ -71,52 +94,33 @@ imdb_actors <- cbind(imdb_title_id = rownames(imdb_actors), imdb_actors)
 imdb_movies <- left_join(imdb_movies,imdb_actors)
 
 lead_reg_df <- imdb_movies_gen %>% 
-  select(-imdb_title_id,-title,-writer,-original_title,-year,-genre,-date_published,-duration,-country,-language,-actors,-description,-avg_vote,-votes,-budget,-usa_gross_income,-metascore,-reviews_from_users,-reviews_from_critics,-genre2,-genre3,-genre4,-worlwide_gross_income)
+  select(-imdb_title_id,-title,-writer,-original_title,-year,-genre,-date_published,-duration,-country,-language,-actors,-description,-avg_vote,-votes,-budget,-usa_gross_income,-metascore,-reviews_from_users,-reviews_from_critics,-genre2,-genre3,-genre4,-worlwide_gross_income, -usacheck)
+
+lead_merge <- lead_reg_df %>% 
+  select(-supporting) %>% 
+  rename('actor'='lead')
+sup_merge <-lead_reg_df %>% 
+  select(-lead) %>% 
+  rename('actor'='supporting')
+
+actor_reg_df <- rbind(lead_merge, sup_merge)
 
 #big regression o'clock
-lm_lead2 <- lm(worldwide_gross ~ ., data = lead_reg_df)
+lm_lead2 <- lm(worldwide_gross ~ ., data = actor_reg_df)
 lm_anova2 <- anova(lm_lead2)
 lm_summary2 <- summary(lm_lead2)
 lead_coef2 <- summary(lm_lead2)$coefficients
 lead_coef2 <- cbind(lead = rownames(lead_coef2), lead_coef2) 
 
+# I think what I need to do is break out the top 8 actors and then only look at the top 100 actors by # of films in. Then do the regression on that
 
+### Clear ----
 
-
-
-
-
-
-
-
-# #I want to see if certain movies are affected by the personal life (num divorces) of the associated actors
-# imdb_names <- read.csv(paste(setpath,"imdb names.csv", sep="/"), header = TRUE)
-# imdb_ratings <- read.csv(paste(setpath,"imdb ratings.csv", sep="/"), header = TRUE)
-# imdb_principals <- read.csv(paste(setpath,"imdb title_principals.csv", sep="/"), header = TRUE)
-# 
-# 
-# #credits are stored as a list of a list - needs extraction
-# credits <- read.csv(paste(setpath,"credits.csv", sep="/"), header = TRUE)
-# # fuck the rest of these datasets. I'm only doing the imdb dataset
-# keywords <- read.csv(paste(setpath,"keywords.csv", sep="/"), header = TRUE)
-# links <- read.csv(paste(setpath,"links.csv", sep="/"), header = TRUE)
-# links_small <- read.csv(paste(setpath,"links_small.csv", sep="/"), header = TRUE)
-# movies_metadata <- read.csv(paste(setpath,"movies_metadata.csv", sep="/"), header = TRUE)
-# netflix_titles <- read.csv(paste(setpath,"netflix_titles.csv", sep="/"), header = TRUE)
-# ratings <- read.csv(paste(setpath,"ratings.csv", sep="/"), header = TRUE)
-# ratings_small <- read.csv(paste(setpath,"ratings_small.csv", sep="/"), header = TRUE)
-# tmdb_credits <- read.csv(paste(setpath,"tmdb_5000_credits.csv", sep="/"), header = TRUE)
-# tmdb_movies <- read.csv(paste(setpath,"tmdb_5000_movies.csv", sep="/"), header = TRUE)
-
-
-export(imdb_movies,file="C:/Users/JosephEmsley/Dropbox (AMEND Consulting)/AMEND Internal Business/So What/fRanken.xlsx")
-export(lead_coef,file="C:/Users/JosephEmsley/Dropbox (AMEND Consulting)/AMEND Internal Business/So What/fRankenleadslm.xlsx")
-
-### clear global environment
+# clear global environment
 rm(list=ls())
 
-####Clear Packages
+# unload Packages
 p_unload(all)
 
-#####Clear Console
+# Clear Console
 cat('\014')
